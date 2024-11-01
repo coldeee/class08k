8b0bbc8fd46b56e5d06286a7db86278ab5e955d4
colde <kiknyt1990@gmail.com>
2024-11-01 15:37:40 +0300

    main

--- Стенография.py
+++ Стенография.py
@@ -50,3 +50,25 @@ if __name__ == "__main__":
         embed_data(image_path, encrypted_data, output_image_path)
     except Exception as e:
         print(f"Произошла ошибка: {e}")
+
+import hashlib
+
+def compute_sha256(file_path):
+    sha256 = hashlib.sha256()
+    with open(file_path, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            sha256.update(chunk)
+    return sha256.hexdigest()
+
+def compute_sha256_after(input_string):
+    sha256 = hashlib.sha256()
+    sha256.update(input_string.encode('utf-8'))
+    return sha256.hexdigest()
+
+
+
+file_checksum = compute_sha256(image_path)
+after_checksum = compute_sha256_after(output_image_path)
+
+print(f"Контрольная сумма исходных данных: {file_checksum}")
+print(f"Контрольная сумма после расшифровки: {after_checksum}")
